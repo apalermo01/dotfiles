a07c6aba812d04c7381890e65d6a8c2e9d10ad04
Alex Palermo <alex.palermo.45@gmail.com>
2025-04-06 21:03:23 -0400

    rearranged themes

---
+++ installs/arch.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+sudo pacman -S yay \
+               python \
+               pyright \
+               flake8 \
+               go \
+               rust \
+               gdb \
+               lua \
+               lua-language-server \
+               
+               i3-wm \
+               i3lock \
+               feh \
+               kde \
+
+               fish \
+               kitty \
+
+               fzf \
+
+               firefox \
+               brave \
+               zen \
+
+               htop \
+               btop \
+               cbonsai \
+               cowsay \
+               fortune \
+               lolcat
+
+

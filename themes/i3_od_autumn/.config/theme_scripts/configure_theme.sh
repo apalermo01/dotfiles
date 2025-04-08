7236038be8e395c608c593228d64082a2481826b
Alex Palermo <alex.palermo@level.agency>
2025-04-08 15:00:37 -0400

    theme builder - 2025-04-08 - i3_od_autumn

---
+++ themes/i3_od_autumn/.config/theme_scripts/configure_theme.sh
@@ -0,0 +1,24 @@
+#!/bin/fish
+# kitty +kitten themes --reload-in=all catppuccin-macchiato
+fisher install rkbk60/onedark-fish
+fish_config prompt save acidhub
+
+set -l onedark_options '-b'
+
+if set -q VIM
+    # Using from vim/neovim.
+    set onedark_options "-256"
+else if string match -iq "eterm*" $TERM
+    # Using from emacs.
+    function fish_title; true; end
+    set onedark_options "-256"
+end
+
+set_onedark $onedark_options
+
+
+if test -e ~/.config/polybar/i3_polybar_start.sh
+    bash ~/.config/polybar/i3_polybar_start.sh
+end
+
+

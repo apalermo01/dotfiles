f9da1b16d2c6e86501eb59af324e439a30990196
Alex Palermo <alex.palermo@level.agency>
2025-04-09 12:55:01 -0400

    theme builder - 2025-04-09 - wsl_dracula

--- themes/wsl_dracula/.config/fish/config.fish
+++ themes/wsl_dracula/.config/fish/config.fish
@@ -90,5 +90,5 @@ abbr --add o ~/appimages/Obsidian-1.7.7.AppImage
 
 set -U OBSIDIAN_NOTES_DIR /mnt/c/Users/apalermo/github/notes/
 set -U NOTES_DIR /mnt/c/Users/apalermo/github/notes/
-fish ~/work_cmds.fish
+source ~/work_cmds.fish
 fastfetch

6ee6ecffd2d74802933b0a6fcba80fcaa543f220
Alex Palermo <alex.palermo@level.agency>
2025-04-09 12:55:03 -0400

    theme builder - 2025-04-09 - wsl_rose_pine

--- themes/wsl_rose_pine/.config/fish/config.fish
+++ themes/wsl_rose_pine/.config/fish/config.fish
@@ -91,5 +91,5 @@ abbr --add o ~/appimages/Obsidian-1.7.7.AppImage
 set -U OBSIDIAN_NOTES_DIR /mnt/c/Users/apalermo/github/notes/
 set -U NOTES_DIR /mnt/c/Users/apalermo/github/notes/
 fish_config theme choose "Rosé Pine"
-fish ~/work_cmds.fish
+source ~/work_cmds.fish
 fastfetch

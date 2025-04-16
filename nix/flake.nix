c7b7fd05038707b041ec324c826211d1d0fca070
= <alex.palermo.ai@gmail.com>
2025-04-16 07:54:33 -0400

    added first pass at nix configs

---
+++ nix/flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    home-manager = {
+       url = "github:nix-community/home-manager";
+       inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs:
+    let
+      system = "x86_64-linux";
+    in
+    {
+      nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+        specialArgs = { inherit inputs; };
+        modules = [
+	  ./configuration.nix
+          inputs.home-manager.nixosModules.home-manager
+	];
+	system = system;
+      };
+    };
+ 
+}

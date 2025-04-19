dce408b433e5d98d7993f19ffad522aa052e1af1
Alex <alex.palermo.ai@gmail.com>
2025-04-19 15:01:20 -0400

    updated bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
+#!/usr/bin/env bash
 
 sudo nixos-rebuild switch \
     --flake github:apalermo01/dotfiles/nix#nixos
--- nix-bootstrap.sh
+++ nix-bootstrap.sh
@@ -1,5 +1,48 @@
 #!usr/bin/env sh
 
+function clone_dotfiles {
+    mkdir -p ~/Documents/git
+    cd ~/Documents/git
+    echo "cloning dotfiles repo"
+    nix-shell -p git --command "
+        git clone git@github.com:apalermo01/dotfiles
+    "
+    cd dotfiles
+    nix-shell -p git --command "
+        git submodule update --init
+    "
+    echo "Dotfiles repo has been cloned and installed."
+
+}
+
+function init_system {
+    echo "available hosts: $(ls nix/hosts)"
+    read -p "Select host: " hostname
+    if [[ ! -d ~/Documents/git/dotfiles/nix/hosts/$hostname ]]; then 
+        echo "Host $hostname does not exist. Exiting..."
+        exit
+    fi
+    echo "Installing system..."
+    sudo nixos-rebuild switch --flake .#vm
+}
+
+read -p "This script expects an ssh key for git to be established. Continue (y/n)?" choice
+case "$choice" in 
+    y|Y) echo "Continuing..." ;;
+    *) echo "exiting" 
+       exit;;
+esac
+
+if [[ ! -d ~/Documents/git/dotfiles ]]; then
+    clone_dotfiles
+fi
+
+read -p "Init system (y/n)?" choice
+case "$choice" in 
+    y|Y) init_system ;;
+    *) echo "continuing";;
+esac
+
 # installing theme builder
 echo "Installing theme builder..."
 bash ./scripts/install_theme_builder.sh

ee5c95240ab365e9d8eb2578a485ab343c38dbb6
Alex Palermo <alex.palermo.45@gmail.com>
2025-04-20 15:10:14 -0400

    updated backup scripts

--- backup/backup.sh
+++ backup/backup.sh
@@ -3,12 +3,6 @@ set -euo pipefail
 
 notify-send "Restic Backup" "Backup is starting..."
 
-MOUNT_PATH="$HOME/mnt/proton"
-if ! mountpoint -q "$MOUNT_PATH" || [ ! -r "$MOUNT_PATH/keys" ]; then
-  echo "[ERROR] Proton Drive is not mounted. Skipping backup." >&2
-  exit 1
-fi
-
 restic backup /home/alex/ \
   --exclude '**/.cache' \
   --exclude '**/node_modules' \
--- backup/decrypt-password.sh
+++ backup/decrypt-password.sh
@@ -2,6 +2,7 @@
 set -euo pipefail
 CONFIG_DIR="$HOME/.config/restic-setup"
 REMOTE_NAME=$(<"$CONFIG_DIR/remote-name.txt")
+
 PASSWORD_FILE="$CONFIG_DIR/restic-password.gpg"
 RCLONE_CONF_GPG="$CONFIG_DIR/rclone.conf.gpg"
 
@@ -13,6 +14,8 @@ TMP_RCLONE_CONF=$(mktemp)
 gpg --quiet --decrypt "$RCLONE_CONF_GPG" > "$TMP_RCLONE_CONF"
 chmod 600 "$TMP_RCLONE_CONF"
 export RCLONE_CONFIG="$TMP_RCLONE_CONF"
+
+export RESTIC_REPOSITORY="rclone:${REMOTE_NAME}:${REMOTE_NAME}"
 
 # Call wrapped command and clean up afterward
 trap 'rm -f "$TMP_RCLONE_CONF"' EXIT

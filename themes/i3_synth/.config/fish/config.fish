e1f4aea63c514019d16363c43371a661595072dd
Alex <alex.palermo.ai@gmail.com>
2025-04-20 14:08:56 -0400

    theme builder - 2025-04-20 - i3_synth

--- themes/i3_synth/.config/fish/config.fish
+++ themes/i3_synth/.config/fish/config.fish
@@ -87,5 +87,6 @@ abbr --add notes cd ~/Documents/git/notes/
 abbr --add zen /usr/local/bin/zen/zen
 abbr --add a . env/bin/activate.fish
 abbr --add o ~/appimages/Obsidian-1.7.7.AppImage
+abbr --add backup-status ~/backup_status.sh
 fortune | cowsay -f $(ls /usr/share/cowsay/cows/ | shuf -n1)
 wal -n -e -i /home/alex/Pictures/wallpapers/synth.jpg > /dev/null 

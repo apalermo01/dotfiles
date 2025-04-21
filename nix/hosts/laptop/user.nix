bb88f4f6bbbf914d3f672a280f15975ce1ae69c5
Alex <alex.palermo.ai@gmail.com>
2025-04-21 00:55:39 -0400

    added config for bare metal

---
+++ nix/hosts/laptop/user.nix
@@ -0,0 +1,15 @@
+{ config, lib, inputs, ... }:
+
+{
+    imports = [ 
+        ../../modules/home/default.nix 
+    ];
+
+    # define what will be installed by home manager here
+    config.modules = {
+      packages.headless.enable = true;
+      packages.ui.enable = true;
+      git.enable = true;
+    };
+    
+}

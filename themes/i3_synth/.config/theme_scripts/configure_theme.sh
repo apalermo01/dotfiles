729b9ee5bea6f75b4c2a69429188165502b22118
Alex Palermo <alex.palermo@level.agency>
2025-05-01 10:47:44 -0400

    theme builder - 2025-05-01 - i3_synth

--- themes/i3_synth/.config/theme_scripts/configure_theme.sh
+++ themes/i3_synth/.config/theme_scripts/configure_theme.sh
@@ -1,9 +1,8 @@
-#!/usr/bin/env sh
-###!/usr/bin/env fish
+#!/bin/fish
 # kitty +kitten themes --reload-in=all catppuccin-macchiato
 # fisher install dracula/fish
 # fish_config theme choose "Dracula Official"
-# fish_config prompt save terlar
+fish_config prompt save terlar
 
 if test -e ~/.config/polybar/i3_polybar_start.sh
     bash ~/.config/polybar/i3_polybar_start.sh

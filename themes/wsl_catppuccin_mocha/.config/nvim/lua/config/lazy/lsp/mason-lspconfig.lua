2d94fe6efd5ef78456d4da48e882511bce1f70fb
Alex <alex.palermo.ai@gmail.com>
2025-05-03 18:04:24 -0400

    theme builder - 2025-05-03 - wsl_catppuccin_mocha

--- themes/wsl_catppuccin_mocha/.config/nvim/lua/config/lazy/lsp/mason-lspconfig.lua
+++ themes/wsl_catppuccin_mocha/.config/nvim/lua/config/lazy/lsp/mason-lspconfig.lua
@@ -8,7 +8,7 @@ return {
 
 		automatic_installation = not nixos,
 		ensure_installed = nixos and {
-			"lua_ls",
+			-- "lua_ls",
 			"html",
 			"cssls",
 			"clangd",
@@ -18,7 +18,7 @@ return {
 			"nil_ls",
 			"bashls",
 		} or {
-			"lua_ls",
+			-- "lua_ls",
 			"html",
 			"cssls",
 			"clangd",

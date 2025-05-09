925b02244859103330d2524979d01662ae0d7d4c
Alex <alex.palermo.ai@gmail.com>
2025-05-09 11:08:59 -0400

    theme builder - 2025-05-09 - i3_catppuccin_macchiato

--- themes/i3_catppuccin_macchiato/.config/install_theme.sh
+++ themes/i3_catppuccin_macchiato/.config/install_theme.sh
@@ -7,10 +7,9 @@ kitty +kitten themes --reload-in=all catppuccin-macchiato
 #fish_config theme save "Catppuccin Mocha"
 # fish_config theme save "Catppuccin Macchiato"
 # fish_config prompt save terlar
-
-if test -e ~/.config/polybar/i3_polybar_start.sh
+if [ -f ~/.config/polybar/i3_polybar_start.sh ]; then
     bash ~/.config/polybar/i3_polybar_start.sh
-end
+fi
 
 
 # TODO: check distro before running these

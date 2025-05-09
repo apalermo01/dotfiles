f25d924679935b60e7b693a64ab7f18f9a7c018f
Alex <alex.palermo.ai@gmail.com>
2025-05-09 11:09:21 -0400

    theme builder - 2025-05-09 - i3_synth

--- themes/i3_synth/.config/install_theme.sh
+++ themes/i3_synth/.config/install_theme.sh
@@ -7,7 +7,10 @@ feh --bg-fill /home/alex/Pictures/wallpapers/synth.jpg
 # fish_config theme choose "Dracula Official"
 # fish_config prompt save terlar
 
-if test -e ~/.config/polybar/i3_polybar_start.sh
-    bash ~/.config/polybar/i3_polybar_start.sh
-end
+# if test -e ~/.config/polybar/i3_polybar_start.sh
+#     bash ~/.config/polybar/i3_polybar_start.sh
+# end
 
+if [ -f ~/.config/polybar/i3_polybar_start.sh ]; then
+    bash ~/.config/polybar/i3_polybar_start.sh
+fi

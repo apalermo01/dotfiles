9db5699ed76ce696b2d8c3d41388bb843226fdc9
Alex <alex.palermo.ai@gmail.com>
2025-05-09 06:21:08 -0400

    theme builder - 2025-05-09 - i3_dracula

--- themes/i3_dracula/.config/install_theme.sh
+++ themes/i3_dracula/.config/install_theme.sh
@@ -3,3 +3,12 @@
 
 
 feh --bg-fill /home/alex/Pictures/wallpapers/dracula_tree.png
+# fisher install dracula/fish
+# fish_config theme choose "Dracula Official"
+# fish_config prompt save acidhub
+
+if [ -f ~/.config/polybar/i3_polybar_start.sh ]; then
+    bash ~/.config/polybar/i3_polybar_start.sh
+fi
+
+

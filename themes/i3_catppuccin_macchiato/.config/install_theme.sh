e3c8a9eac2a60b4451139399d82db9dfcca07ac7
Alex <alex.palermo.ai@gmail.com>
2025-05-09 07:06:44 -0400

    theme builder - 2025-05-09 - i3_catppuccin_macchiato

---
+++ themes/i3_catppuccin_macchiato/.config/install_theme.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+feh --bg-fill /home/alex/Pictures/wallpapers/darker_unicat.png
+## !/usr/bin/env fish
+kitty +kitten themes --reload-in=all catppuccin-macchiato
+# fisher install catppuccin/fish
+#fish_config theme save "Catppuccin Mocha"
+# fish_config theme save "Catppuccin Macchiato"
+# fish_config prompt save terlar
+
+if test -e ~/.config/polybar/i3_polybar_start.sh
+    bash ~/.config/polybar/i3_polybar_start.sh
+end
+
+
+# TODO: check distro before running these
+
+# read -p "Install fish shell (y/n)?" choice
+# case "$choice" in 
+#     y|Y) echo "installing fish shell..." && sudo pacman -S fish;;
+# esac
+
+read -p "Install maple font? (y/n)?" choice
+case "$choice" in 
+    y|Y) echo "installing maple mono font..." && yay -S ttf-maple;;
+esac

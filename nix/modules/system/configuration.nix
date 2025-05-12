81ae74e92cbe944def8d595d6e19b72f02ed8bb7
Alex <alex.palermo.ai@gmail.com>
2025-05-12 19:51:40 -0400

    commit from nix system update

--- nix/modules/system/configuration.nix
+++ nix/modules/system/configuration.nix
@@ -130,6 +130,7 @@
   services.gnome.gnome-keyring.enable = true;
   security.pam.services.sddm.enableGnomeKeyring = true;
   security.pam.services.login.enableGnomeKeyring = true;
+  security.pam.services.i3lock.enable = true;
 
   # Configure keymap in X11
   services.xserver.xkb = {

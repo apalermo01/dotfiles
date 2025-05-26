4df2d79f56a497bd7d206c1dc1de4db1c51273ef
Alex <alex.palermo.ai@gmail.com>
2025-05-26 11:31:55 -0400

    fix obsidian handler config

--- nix/modules/home/desktop-entries.nix
+++ nix/modules/home/desktop-entries.nix
@@ -1,18 +1,30 @@
-{ pkgs, lib, config, ... }:
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}:
 
 with lib;
-let cfg = config.modules.desktop-entries;
+let
+  cfg = config.modules.desktop-entries;
 
-in {
-    options.modules.desktop-entries = { enable = mkEnableOption "desktop-entries"; };
-    config = mkIf cfg.enable {
-      # configure obsidian
-      xdg.desktopEntries.obisidan = {
-        exec = "executable %u";
-        name = "/etc/profiles/per-user/alex/bin/obsidian %u";
-        type = "Application";
-        mimeType = ["ASCII text"];
-      };
+in
+{
+  options.modules.desktop-entries = {
+    enable = mkEnableOption "desktop-entries";
+  };
+  config = mkIf cfg.enable {
+    # configure obsidian
+    xdg.desktopEntries.obsidian = {
+      name = "Obsidian";
+      exec = "/etc/profiles/per-user/alex/bin/obsidian %u";
+      type = "Application";
+      mimeType = [
+        "x-scheme-handler/obsidian"
+        "text/markdown"
+      ];
+    };
 
   };
 }

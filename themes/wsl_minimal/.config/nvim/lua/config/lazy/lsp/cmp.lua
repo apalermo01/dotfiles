0da3b970dbc2e7c93bc878a48f951db144415db0
Alex Palermo <alex.palermo@level.agency>
2025-05-27 11:35:25 -0400

    theme builder - 2025-05-27 - wsl_minimal

--- themes/wsl_minimal/.config/nvim/lua/config/lazy/lsp/cmp.lua
+++ themes/wsl_minimal/.config/nvim/lua/config/lazy/lsp/cmp.lua
@@ -78,12 +78,11 @@ return {
 				["<C-k>"] = cmp.mapping.scroll_docs(-4),
 				["<C-j>"] = cmp.mapping.scroll_docs(4),
 				["<C-e>"] = cmp.mapping.abort(),
-				["<CR>"] = cmp.mapping.abort(),
 				["<C-o>"] = cmp.mapping.open_docs(),
 				["<TAB>"] = cmp.mapping.select_next_item(cmp_select),
 				["<S-TAB>"] = cmp.mapping.select_prev_item(cmp_select),
 				["<C-y>"] = cmp.mapping.confirm({ select = true }),
-				-- ["<CR>"] = cmp.mapping.confirm({ select = true }),
+				["<CR>"] = cmp.mapping.confirm({ select = true }),
 				["<C-space>"] = cmp.mapping.complete(),
 			}),
 

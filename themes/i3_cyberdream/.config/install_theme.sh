390203be6aff5d69d1217d1a5d9634fbf8167661
Alex <alex.palermo.ai@gmail.com>
2025-06-01 15:34:16 -0400

    theme builder - 2025-06-01 - i3_cyberdream

---
+++ themes/i3_cyberdream/.config/install_theme.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+feh --bg-fill /home/alex/Pictures/wallpapers/wallhaven-zy3l5o.jpg
+###!/usr/bin/env fish
+# fish_config prompt save acidhub
+# fish_config theme save cyberdream
+
+if [ ! -d ~/.config/tmux/plugins/catppuccin ]; then
+    mkdir -p ~/.config/tmux/plugins/catppuccin
+    git clone -b v2.1.3 https://github.com/catppuccin/tmux.git ~/.config/tmux/plugins/catppuccin/tmux
+fi
+
+# if test -e ~/cyberdream.conf
+#     mv ~/cyberdream.conf ~/.config/tmux/plugins/catppuccin/tmux/themes/catppuccin_cyberdream_tmux.conf
+# end
+
+kitten themes --reload-in=all cyberdream
+
+if [ -f ~/.config/polybar/i3_polybar_start.sh ]; then
+    bash ~/.config/polybar/i3_polybar_start.sh
+fi
+

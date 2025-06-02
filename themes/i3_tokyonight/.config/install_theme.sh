284b7be699ef38edcd14538ddb6f6dce84dc269b
Alex Palermo <alex.palermo@level.agency>
2025-06-02 09:40:21 -0400

    theme builder - 2025-06-02 - i3_tokyonight

---
+++ themes/i3_tokyonight/.config/install_theme.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+feh --bg-fill /home/apalermo/Pictures/wallpapers/spookyjs_upscayl_realesrgan-x4plus_x2.png
+###!/usr/bin/env fish
+feh --bg-fill $HOME/Pictures/wallappers/spookyjs_upscayl_realesrgan-x4plus_x2.png
+               
+# fish_config prompt save acidhub
+# if test -e ~/.config/polybar/i3_polybar_start.sh
+#     bash ~/.config/polybar/i3_polybar_start.sh
+# end
+
+if [ -f ~/.config/polybar/i3_polybar_start.sh ]; then
+    bash ~/.config/polybar/i3_polybar_start.sh
+fi

098413cbf63db2e0102328bcc206bba63d4940b8
Alex Palermo <alex.palermo@level.agency>
2025-06-02 09:40:19 -0400

    theme builder - 2025-06-02 - i3_rose_pine

---
+++ themes/i3_rose_pine/.config/install_theme.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+plasma-apply-colorscheme RosePineMoon
+feh --bg-fill /home/apalermo/Pictures/wallpapers/leafy.png
+# fisher install rose-pine/fish
+# fish_config prompt save acidhub
+# fish_config theme choose "Rosé Pine"
+
+kitty +kitten themes --reload-in=all Rosé Pine
+
+if [ -f ~/.config/polybar/i3_polybar_start.sh ]; then
+    bash ~/.config/polybar/i3_polybar_start.sh
+fi
+
+# if test -e ~/.config/polybar/i3_polybar_start.sh
+#     bash ~/.config/polybar/i3_polybar_start.sh
+# end
+

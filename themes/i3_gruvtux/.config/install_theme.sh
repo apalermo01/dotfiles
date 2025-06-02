dba31f428f383c256a42230ee88a6c0a62f51062
Alex Palermo <alex.palermo@level.agency>
2025-06-02 09:40:17 -0400

    theme builder - 2025-06-02 - i3_gruvtux

---
+++ themes/i3_gruvtux/.config/install_theme.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+plasma-apply-colorscheme Gruvbox
+feh --bg-fill /home/apalermo/Pictures/wallpapers/gruvtux.png
+###!/usr/bin/env fish
+# kitty +kitten themes --reload-in=all catppuccin-macchiato
+# fisher install jomik/fish-gruvbox
+# fish_config prompt save acidhub
+bash ~/.config/polybar/i3_polybar_start.sh
+
+if test -e ~/.config/polybar/i3_polybar_start.sh
+    bash ~/.config/polybar/i3_polybar_start.sh
+end
+

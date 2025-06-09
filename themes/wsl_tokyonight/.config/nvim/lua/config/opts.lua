db8bd4530ea3bebd738f94e83e2ce836d2803472
Alex Palermo <alex.palermo@level.agency>
2025-06-09 10:15:41 -0400

    theme builder - 2025-06-09 - wsl_tokyonight

--- themes/wsl_tokyonight/.config/nvim/lua/config/opts.lua
+++ themes/wsl_tokyonight/.config/nvim/lua/config/opts.lua
@@ -72,7 +72,6 @@ local border = {
 
 vim.cmd([[let g:markdown_folding=1]])
 vim.cmd([[set nofoldenable]])
-vim.notify = require('notify')
 -- folding behavior
 
 -- add binaries installed by mason.nvim to path

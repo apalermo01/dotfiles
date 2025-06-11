bc12c60bc21b873576f778aff8fb96dfef25b330
Alex <alex.palermo.ai@gmail.com>
2025-06-11 18:08:11 -0400

    install discord, update theme builder

--- nix/modules/home/discord.nix
+++ nix/modules/home/discord.nix
@@ -1,14 +1,21 @@
-{ pkgs, lib, config, ... }:
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}:
 
 with lib;
-let cfg = config.modules.discord;
+let
+  cfg = config.modules.discord;
 
-in {
-    
-    options.modules.discord = { enable = mkEnableOption "discord"; };
-    config = mkIf cfg.enable {
-        programs.discord = {
-            enable = true;
-        };
-    };
+in
+{
+
+  options.modules.discord = {
+    enable = mkEnableOption "discord";
+  };
+  config = mkIf cfg.enable {
+    home.packages = [ pkgs.discord ];
+  };
 }

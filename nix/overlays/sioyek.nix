45c7c75e2f673b7542c0e2e515e78d78b7ec0bf0
Alex <alex.palermo.ai@gmail.com>
2025-10-13 00:27:26 -0400

    commit from nix system update

--- nix/overlays/sioyek.nix
+++ nix/overlays/sioyek.nix
@@ -1,25 +1,60 @@
-self: super:
-let 
-  py = super.python313;
-
-in 
+final: prev:
+let
+  py = prev.python313;
+in
 {
-  sioyek = py.pkgs.buildPythonPackage rec {
-    pname = "sioyek";
-    format = "pyproject";
-    version = "0.31.11";
+  python313 = prev.python313.override {
+    packageOverrides = python-final: python-prev: {
+      sioyek = python-prev.buildPythonPackage rec {
+        pname = "sioyek";
+        version = "0.31.11";
+        format = "pyproject";
 
-    src = fetchPypi {
-      inherit pname version;
-      hash = "";
+        src = prev.fetchPypi {
+          inherit pname version;
+          sha256 = "sha256-KzrwK21CU80rT2Stns3Iuly2+83w6MXPUdTLkqu9S+M=";
+        };
+
+        nativeBuildInputs = with python-prev; [
+          hatchling
+        ];
+
+        propagatedBuildInputs = with python-prev; [
+          requests
+          appdirs
+          numpy
+          pymupdf
+          pyperclip
+          pyqt5
+          python-slugify
+          regex
+        ];
+        dontCheckRuntimeDeps = true;
+        doCheck = false;
+
+        pythonImportsCheck = [ "sioyek" ];
+
+        meta = with prev.lib; {
+          description = "Python tools and extensions for Sioyek PDF reader";
+          homepage = "https://pypi.org/project/sioyek/";
+          license = licenses.gpl3Plus;
+          maintainers = [ ];
+        };
+      };
     };
-
-    doCheck = false;
-
-    pyproject = true;
-    build-system = [
-      setuptools
-      wheel
-    ];
   };
+  # prev.pkgs.python313.withPackages = {
+  #   sioyek = py.pkgs.buildPythonPackage rec {
+  #     pname = "sioyek";
+  #     version = "0.31.11";
+  #     format = "pyproject";
+  #
+  #     src = prev.fetchPypi {
+  #       inherit pname version;
+  #       sha256 = "";
+  #
+  #     };
+  #     doCheck = false;
+  #   };
+  # };
 }

d94c075b4333f2f5373cd45acd7127c356de2757
Alex <alex.palermo.ai@gmail.com>
2025-11-08 01:06:50 -0500

    commit from nix system update

--- nix/roles/desktop.nix
+++ nix/roles/desktop.nix
@@ -107,47 +107,47 @@ in
     # pgformatter
   ];
 
-  services.usbmuxd.enable = true;
-  virtualisation.docker = {
-    enable = true;
-  };
-
-  programs.virt-manager.enable = true;
-  users.groups.libvirtd.members = [ "alex" ];
-  virtualisation.libvirtd.enable = true;
-  virtualisation.spiceUSBRedirection.enable = true;
-
-  programs.hyprland.enable = true;
-  environment.sessionVariables.NIXOS_OZONE_WL = "1";
-
-  programs.nix-ld.enable = true;
-  programs.kdeconnect.enable = true;
-
-  # networking
-  networking = {
-    networkmanager = {
-      enable = true;
-      plugins = [ pkgs.networkmanager-openvpn ];
-    };
-    nameservers = [
-      "1.1.1.1"
-      "1.0.0.1"
-      "8.8.8.8"
-    ];
-  };
-
-  programs.gnupg.agent = {
-    enable = true;
-    pinentryPackage = pkgs.pinentry-qt;
-    enableSSHSupport = true;
-  };
-
-  services.udev.packages = [ pkgs.via ];
-  hardware.keyboard.qmk.enable = true;
+  # services.usbmuxd.enable = true;
+  # virtualisation.docker = {
+  #   enable = true;
+  # };
+  #
+  # programs.virt-manager.enable = true;
+  # users.groups.libvirtd.members = [ "alex" ];
+  # virtualisation.libvirtd.enable = true;
+  # virtualisation.spiceUSBRedirection.enable = true;
+  #
+  # programs.hyprland.enable = true;
+  # environment.sessionVariables.NIXOS_OZONE_WL = "1";
+  #
+  # programs.nix-ld.enable = true;
+  # programs.kdeconnect.enable = true;
+  #
+  # # networking
+  # networking = {
+  #   networkmanager = {
+  #     enable = true;
+  #     plugins = [ pkgs.networkmanager-openvpn ];
+  #   };
+  #   nameservers = [
+  #     "1.1.1.1"
+  #     "1.0.0.1"
+  #     "8.8.8.8"
+  #   ];
+  # };
+  #
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   pinentryPackage = pkgs.pinentry-qt;
+  #   enableSSHSupport = true;
+  # };
+  #
+  # services.udev.packages = [ pkgs.via ];
+  # hardware.keyboard.qmk.enable = true;
 
   # default KDE environment. Can use as a fallback
   # if something happens to i3 or hyprland
-  services.desktopManager.plasma6.enable = false;
+  # services.desktopManager.plasma6.enable = false;
 
   # x-server. Can disable if only using wayland
   # services.displayManager.defaultSession = "plasmax11";
@@ -158,65 +158,65 @@ in
   # services.displayManager.sddm.enable = true;
   # services.displayManager.defaultSession = "none+i3";
 
-  hardware.bluetooth.enable = true;
-  hardware.bluetooth.powerOnBoot = true;
-  services.blueman.enable = true;
-  services.pipewire = {
-    enable = true;
-    audio.enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    jack.enable = true;
-  };
+  # hardware.bluetooth.enable = true;
+  # hardware.bluetooth.powerOnBoot = true;
+  # services.blueman.enable = true;
+  # services.pipewire = {
+  #   enable = true;
+  #   audio.enable = true;
+  #   alsa.enable = true;
+  #   alsa.support32Bit = true;
+  #   pulse.enable = true;
+  #   jack.enable = true;
+  # };
+  #
+  # security.pam.services.i3lock.enable = true;
+  #
+  # services.logind.settings.Login = {
+  #   HandleLidSwitchDocked = "lock";
+  #   HandleLidSwitchExternalPower = "lock";
+  #   HandleLidSwitch = "lock";
+  # };
+  #
+  # environment.etc."xdg/menus/plasma-applications.menu".source =
+  #   "${pkgs.kdePackages.plasma-workspace}/etc/xdg/menus/plasma-applications.menu";
+  #
+  # services.power-profiles-daemon.enable = false;
 
-  security.pam.services.i3lock.enable = true;
-
-  services.logind.settings.Login = {
-    HandleLidSwitchDocked = "lock";
-    HandleLidSwitchExternalPower = "lock";
-    HandleLidSwitch = "lock";
-  };
-
-  environment.etc."xdg/menus/plasma-applications.menu".source =
-    "${pkgs.kdePackages.plasma-workspace}/etc/xdg/menus/plasma-applications.menu";
-
-  services.power-profiles-daemon.enable = false;
-
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
-  services.avahi = {
-    enable = true;
-    nssmdns4 = true;
-    openFirewall = true;
-  };
-
-  security.rtkit.enable = true;
-
-  # users
-  users.users.alex = {
-    isNormalUser = true;
-    extraGroups = [
-      "wheel"
-      "networkmanager"
-      "docker"
-
-    ];
-    shell = pkgs.zsh;
-  };
-
-  nix = {
-    settings.auto-optimise-store = true;
-    settings.allowed-users = [ "alex" ];
-    gc = {
-      automatic = true;
-      dates = "weekly";
-      options = "--delete-older-than 7d";
-    };
-    extraOptions = ''
-      experimental-features = nix-command flakes
-      keep-outputs = true
-      keep-derivations = true
-    '';
-  };
+  # # Enable CUPS to print documents.
+  # services.printing.enable = true;
+  # services.avahi = {
+  #   enable = true;
+  #   nssmdns4 = true;
+  #   openFirewall = true;
+  # };
+  #
+  # security.rtkit.enable = true;
+  #
+  # # users
+  # users.users.alex = {
+  #   isNormalUser = true;
+  #   extraGroups = [
+  #     "wheel"
+  #     "networkmanager"
+  #     "docker"
+  #
+  #   ];
+  #   shell = pkgs.zsh;
+  # };
+  #
+  # nix = {
+  #   settings.auto-optimise-store = true;
+  #   settings.allowed-users = [ "alex" ];
+  #   gc = {
+  #     automatic = true;
+  #     dates = "weekly";
+  #     options = "--delete-older-than 7d";
+  #   };
+  #   extraOptions = ''
+  #     experimental-features = nix-command flakes
+  #     keep-outputs = true
+  #     keep-derivations = true
+  #   '';
+  # };
 }

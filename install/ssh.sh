82f99bb96651b3e579a6b5c850e439e67cbfecd9
Alex <alex.palermo.ai@gmail.com>
2025-11-16 01:27:06 -0500

    11/15/25

--- install/ssh.sh
+++ install/ssh.sh
@@ -1,28 +1,31 @@
-#!/usr/bin/env bash 
+#!/usr/bin/env bash
 
-make_ssh() {
-    ssh-keygen -t ed25519 -f ~/.ssh/$1
+echo "generating ssh key $1"
+
+ssh-keygen -t ed25519 -f ~/.ssh/$1
+if ! pgrep -u "$USER" ssh-agent >/dev/null; then
     eval "$(ssh-agent -s)"
-    ssh-add ~/.ssh/$1
+fi
+ssh-add ~/.ssh/$1
 
-    case $1 in 
-        github)
-            sed -i '/github/d' ~/.ssh/config
-            cat <<-"EOF" > ~/.ssh/config
+case $1 in
+github)
+    sed -i '/github/d' ~/.ssh/config
+    cat <<EOF >> ~/.ssh/config
 Host github.com 
-    Hostname github.com 
+    HostName github.com 
     IdentityFile ~/.ssh/github
 EOF
-        ;;
-        gitlab)
-            sed -i '/gitlab/d' ~/.ssh/config
-            cat <<-"EOF" > ~/.ssh/config
+    ;;
+gitlab)
+    sed -i '/gitlab/d' ~/.ssh/config
+    cat <<EOF >> ~/.ssh/config
 Host gitlab.com 
-    Hostname gitlab.com 
+    HostName gitlab.com 
     IdentityFile ~/.ssh/gitlab
 EOF
-        ;;
-    esac
-}
+    ;;
+esac
 
-make_ssh
+
+chmod 600 ~/.ssh/config

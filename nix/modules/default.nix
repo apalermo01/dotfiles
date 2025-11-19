e999d476fdf3318e23e136ffa16c2b93dcdf67be
Alex <alex.palermo.ai@gmail.com>
2025-11-18 19:48:54 -0500

    commit from nix system update

--- nix/modules/default.nix
+++ nix/modules/default.nix
@@ -4,6 +4,7 @@
 {
   imports = [
     ./i3.nix
+    ./python.nix
     ./common.nix
     ./external_monitor.nix
   ];
--- nix/modules/python.nix
+++ nix/modules/python.nix
@@ -4,6 +4,16 @@
   config,
   ...
 }:
+
+let 
+
+  python = pkgs.python313.withPackages (ps: [
+    ps.black
+    ps.isort
+    ps.ipython
+    ps.pip
+  ]);
+in
 {
   options = {
     python.enable = lib.mkEnableOption "Enables python";
@@ -12,6 +22,7 @@
   config = lib.mkIf config.python.enable {
     environment.systemPackages = with pkgs; [
       pyright
+      python
     ];
   };
 }

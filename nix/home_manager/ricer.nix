9919668dc5cf4bc40b918ce97fbf47d466f804d4
Alex <alex.palermo.ai@gmail.com>
2025-11-19 01:05:59 -0500

    commit from nix system update

---
+++ nix/home_manager/ricer.nix
@@ -0,0 +1,17 @@
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}:
+{
+  options = {
+    modules.ricer.enable = lib.mkEnableOption "enables custom theme switcher";
+  };
+
+  config = lib.mkIf config.modules.ricer.enable {
+    home.packages = [
+      pkgs.ricer
+    ];
+  };
+}

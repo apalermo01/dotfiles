66cb323c48763aadaba922c3af9db1b53ece1aef
Alex <alex.palermo.ai@gmail.com>
2025-11-26 00:39:42 -0500

    11/26/25

--- nix/home_manager/wm/default.nix
+++ nix/home_manager/wm/default.nix
@@ -3,8 +3,6 @@
 }:
 {
   imports = [
-    ./xss-lock-i3.nix
-    ./xautolock-i3.nix
     ./kde-pkgs.nix
     ./xdg.nix
   ];
--- nix/modules/wm/i3.nix
+++ nix/modules/wm/i3.nix
@@ -24,13 +24,22 @@
     services.xserver = {
       enable = true;
       windowManager.i3.enable = true;
+      xautolock = {
+        enable = true;
+        enableNotifier = true;
+        notifier = "${pkgs.libnotify}/bin/notify-send 'Locking in 30 seconds'";
+        locker = "${pkgs.i3lock}/bin/i3lock";
+        notify = 30;
+        time = 1;
+      };
     };
 
     security.pam.services.i3lock.enable = true;
     services.displayManager.defaultSession = "none+i3";
 
-    # services = {
-    #   xss-lock-i3.enable = true;
-    # };
+    programs.xss-lock = {
+      enable = true;
+      lockerCommand = "${pkgs.i3lock}/bin/i3lock";
+    };
   };
 }

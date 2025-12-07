40218eec589832d68c2b6e4c12e48320ffbd39fd
Alex <alex.palermo.ai@gmail.com>
2025-12-07 13:57:57 -0500

    commit from nix system update

--- nix/modules/external_monitor.nix
+++ nix/modules/external_monitor.nix
@@ -1,13 +1,36 @@
-{ config, pkgs, lib, ... }:
-
 {
-  # options.modules.sddmMonitorLayout.enable = lib.mkEnableOption "Enable monitor layout script for SDDM";
+  config,
+  pkgs,
+  lib,
+  ...
+}:
 
-  # config = lib.mkIf config.modules.sddmMonitorLayout.enable {
-    services.displayManager.sddm.setupScript = ''
-      #!/bin/sh
-      ${pkgs.xorg.xrandr}/bin/xrandr --output HDMI-2 --auto --left-of eDP-1
-      ${pkgs.xorg.xrandr}/bin/xrandr --output DP-1 --auto --left-of HDMI-2
+let
+  MonitorLayout = pkgs.writeShellScript "monitor-layout" ''
+    #!/bin/sh
+    ${pkgs.xorg.xrandr}/bin/xrandr --output HDMI-2 --auto --left-of eDP-1
+    ${pkgs.xorg.xrandr}/bin/xrandr --output DP-1 --auto --left-of HDMI-2
+  '';
+in
+{
+
+  services.displayManager.sddm.setupScript = "${MonitorLayout}";
+
+  services.acpid = {
+    enable = true;
+    lidEventCommands = ''
+        export DISPLAY=:0
+        export PATH=$PATH:/run/current-system/sw/bin
+        export XAUTHORITY=$(ls /var/run/sddm/ | head -n1)
+
+        LID_STATE=$(cat /proc/acpi/button/lid/LID/state | awk '{print $2}')
+        EXTERNAL_MONITOR=$(xrandr | grep " connected" | grep -v "eDP" | awk '{print $1}')
+
+        if [ "$LID_STATE" = "closed" ] && [ -n "$EXTERNAL_MONITOR" ]; then
+          xrandr --output eDP-1 --off --output $EXTERNAL_MONITOR --auto
+        else
+          ${MonitorLayout}
+        fi
     '';
-  # };
+  };
 }

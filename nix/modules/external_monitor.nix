da8d3ab490e6d8df744ae3ef6bd7b4eb3428dc78
Alex <alex.palermo.ai@gmail.com>
2025-12-07 14:19:07 -0500

    commit from nix system update

--- nix/modules/external_monitor.nix
+++ nix/modules/external_monitor.nix
@@ -21,15 +21,17 @@ in
     lidEventCommands = ''
         export DISPLAY=:0
         export PATH=$PATH:/run/current-system/sw/bin
-        export XAUTHORITY=$(ls /var/run/sddm/ | head -n1)
+        # export XAUTHORITY=$(ls /var/run/sddm/ | head -n1)
+        export XAUTHORITY=/var/run/sddm/xauth_edUlZp
         echo "xauthority: $XAUTHORITY"
+
         LID_STATE=$(cat /proc/acpi/button/lid/LID/state | awk '{print $2}')
         EXTERNAL_MONITOR=$(xrandr | grep " connected" | grep -v "eDP" | awk '{print $1}')
 
         if [ "$LID_STATE" = "closed" ] && [ -n "$EXTERNAL_MONITOR" ]; then
           xrandr --output eDP-1 --off --output $EXTERNAL_MONITOR --auto
         else
-          ${MonitorLayout}
+          xrandr --output $EXTERNAL_MONITOR --auto --left-of eDP-1
         fi
     '';
   };

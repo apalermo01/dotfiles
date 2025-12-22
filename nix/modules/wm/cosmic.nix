ef9dfd6aa7ebc2a019592586106285a2896edb8f
Alex <alex.palermo.ai@gmail.com>
2025-12-21 22:34:20 -0500

    12/21/25

--- nix/modules/wm/cosmic.nix
+++ nix/modules/wm/cosmic.nix
@@ -12,6 +12,7 @@
 
   config = lib.mkIf config.cosmic.enable {
     environment.systemPackages = with pkgs; [
+        xorg.xinit # adding this b/c of startx not found error when starting i3 from cosmic-greeter
     ];
 
     services.displayManager.cosmic-greeter.enable = true;
